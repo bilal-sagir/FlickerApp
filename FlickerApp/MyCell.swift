d97eb6fd76014f1f7ff7cf6baf69ce2c82e9f83b
bilal-sagir <sagir.bilal@icloud.com>
2021-12-02 13:16:54 +0300

    set ui

---
+++ FlickerApp/MyCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class MyCell : UICollectionViewCell
+{
+    @IBOutlet weak var imgView: UIImageView!
+    
+    
+}
--- FlickerApp/ViewController.swift
+++ FlickerApp/ViewController.swift
@@ -2,7 +2,15 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    @IBAction func doSearch()
+    {
+        let searchTerm = searchTextField.text!
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
