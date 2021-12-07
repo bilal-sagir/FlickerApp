dcc3aad4537caa07cc88f1ac9fdcad1a478c499f
bilal-sagir <sagir.bilal@icloud.com>
2021-12-07 15:06:30 +0300

    add flickr api

--- FlickerApp/ViewController.swift
+++ FlickerApp/ViewController.swift
@@ -1,5 +1,17 @@
 import UIKit
 
+/*
+ api_key : b61ae7a29725bf8c55e94e28c5a8e3b9
+ 
+ url_sample : https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
+
+ https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\()&text=\()&format=json&nojsoncallback=1
+ 
+ */
+
+
+
+
 class ViewController: UIViewController
 {
     @IBOutlet weak var searchTextField: UITextField!
